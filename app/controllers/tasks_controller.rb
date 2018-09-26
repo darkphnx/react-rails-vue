cf3e279121c19bda2ae37585c0828a30dc35efbe
Dan Wentworth <dan@atechmedia.com>
2018-09-26 19:56:37 +0100

    add tasks controller and a basic index view

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :find_task
+
+  def index
+  end
+
+  def create
+    @task = Task.build(safe_task_params)
+
+    respond_to do |wants|
+      if @task.save
+        wants.json { render json: @task.to_json, status: :created }
+      else
+        wants.json { render json: @task.errors, status: :unprocessible_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |wants|
+      if @task.update(safe_task_params)
+        wants.json { render json: @task.to_json }
+      else
+        wants.json { render json: @task.errors, status: :unprocessible_entity }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |wants|
+      if @task.destroy
+        head :ok
+      else
+        wants.json { render json: @task.errors, status: :unprocessible_entity }
+      end
+    end
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id]) if params[:id]
+  end
+
+  def safe_task_params
+    params.require(:task).permit(:title, :tags_list)
+  end
+end

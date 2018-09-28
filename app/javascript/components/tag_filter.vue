0e8161166009cc11d9e368702dbd32b1eeca336b
Dan Wentworth <dan@atechmedia.com>
2018-09-28 10:42:36 +0100

    tag_filter docs

--- app/javascript/components/tag_filter.vue
+++ app/javascript/components/tag_filter.vue
@@ -26,9 +26,21 @@
       selectedTags: Array
     },
     methods: {
+      /**
+       * Unset any current tag filters by emitting update-selected-tags with
+       * an empty array
+       *
+       * @fires update-selected-tags
+       */
       clearTagFilters: function() {
         this.$emit('update-selected-tags', []);
       },
+      /**
+       * Turn a tag filter on or off depending on it's current state. Emits
+       * an update-selected-tags event with the new tag list
+       *
+       * @fires update-selected-tags
+       */
       toggleTagFilter: function(tag) {
         const currentSelectedTags = this.selectedTags;
         const tagIndex = currentSelectedTags.indexOf(tag);
@@ -43,11 +55,17 @@
 
         this.$emit('update-selected-tags', currentSelectedTags);
       },
+      /**
+       * @returns {String} A label-primary tag if given tag is active
+       */
       tagClass: function(tag) {
         if(this.selectedTags.includes(tag)){
           return 'label-primary';
         }
       },
+      /**
+       * @returns {String} A label-primary tag if no tags are active
+       */
       allTagClass: function() {
         if(this.selectedTags.length === 0){
           return 'label-primary';

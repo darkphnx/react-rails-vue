288f719ce388077df770072d04a8c6462df87daf
Dan Wentworth <dan@atechmedia.com>
2018-09-28 10:34:15 +0100

    comments for empty component

--- app/javascript/components/empty.vue
+++ app/javascript/components/empty.vue
@@ -16,6 +16,12 @@ export default {
     subtitle: String
   },
   computed: {
+    /**
+     * CSS classes to use for the icon. Uses the icon prop to pick from the
+     * list at https://picturepan2.github.io/spectre/elements.html#icons
+     *
+     * @returns {String[]} Array of css classes
+     */
     iconClasses: function (){
       const iconClass = `icon-${this.icon}`;
       return ['icon', iconClass];
